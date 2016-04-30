5e545dff982c9ce735e473181f14f0ee37822479
Jeremy Drake <github@jdrake.com>
2016-04-29 21:19:58 -0700

    remove remnants of blinky, use uart1 for output, swap uart0

--- driver/uart.c
+++ driver/uart.c
@@ -146,6 +146,6 @@ uart_init(UartBautRate uart0_br, UartBautRate uart1_br)
     ETS_UART_INTR_ENABLE();
 
     // install uart1 putc callback
-//    os_install_putc1((void *)uart1_write_char);
+    os_install_putc1((void *)uart1_tx_one_char);
 }
 
--- upscontrol.c
+++ upscontrol.c
@@ -1,48 +1,23 @@
-#include "ets_sys.h"
 #include "osapi.h"
-#include "gpio.h"
-#include "os_type.h"
 #include "user_interface.h"
 #include "cpp_routines.h"
-#include <stdlib.h>
-
-static const int pin = 0;
-static volatile os_timer_t some_timer;
-
-void some_timerfunc(void *arg)
-{
-  //Do blinky stuff
-  if (GPIO_REG_READ(GPIO_OUT_ADDRESS) & (1 << pin))
-  {
-    // set gpio low
-    gpio_output_set(0, (1 << pin), 0, 0);
-  }
-  else
-  {
-    // set gpio high
-    gpio_output_set((1 << pin), 0, 0, 0);
-  }
-}
+#include "driver/uart.h"
 
 void ICACHE_FLASH_ATTR init_done()
 {
 	os_printf("Init done!\n");
-	// setup timer (500ms, repeating)
-	os_timer_setfn(&some_timer, (os_timer_func_t *)some_timerfunc, NULL);
-	os_timer_arm(&some_timer, 500, 1);
 }
 
+// they seem to have neglected to define U0CTS function...
+#define FUNC_U0CTS FUNC_U0RTS
 void ICACHE_FLASH_ATTR user_init()
 {
+	uart_init(2400, 74880);
+	PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTDO_U, FUNC_U0RTS);
+	PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTCK_U, FUNC_U0CTS);
+	system_uart_swap();
   system_set_os_print(1);
-  os_printf("Hello world!\n");
   do_global_ctors();
-  // init gpio sussytem
-  gpio_init();
-
-  // configure UART TXD to be GPIO1, set as output
-  PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO0_U, FUNC_GPIO0);
-  gpio_output_set(0, 0, (1 << pin), 0);
-
+  os_printf("Hello world!\n");
   system_init_done_cb(&init_done);
 }
